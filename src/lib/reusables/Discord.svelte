ca91fcc90f1b9a34e8f59e34ce6d95d0426cdcc2
Davey <meeprdib@outlook.com>
2026-03-24 16:59:55 -0400

    discord stuff cleanup

--- src/lib/reusables/Discord.svelte
+++ src/lib/reusables/Discord.svelte
@@ -4,18 +4,18 @@
 
 <div class="bg-stone-950 w-full min-w-[25%] border-l-4 border-dph-orange p-3 pr-5 my-2 mt-10">
   <div class="flex items-center gap-3">
-    <img alt="Datapack Hub Discord" src="./logos/dph.webp" class="not-prose h-16 m-2 hidden md:block" />
+    <img alt="Modpack Dev Knowledgebase Discord" src="./logos/dph.webp" class="not-prose h-16 m-2 hidden md:block" />
     <div class="flex flex-col not-prose">
       <div class="flex text-sm gap-2 items-center text-orange-400 mb-1">
         <IconDiscord />
         <span>JOIN OUR DISCORD SERVER</span>
       </div>
       <p>
-        If you need any more help with your datapacks, don't hesitate to join the
-        <span class="text-orange-400">Datapack Hub</span> Discord server!
+        If you need any more help with modpack development, don't hesitate to join the
+        <span class="text-orange-400">Modpack Dev Knowledgebase</span> Discord server!
         <br />
         Join us now!
-        <a href="https://discord.gg/xHTHbZqXr6" class="underline text-orange-400">https://discord.gg/xHTHbZqXr6</a>
+        <a href="https://discord.gg/PLACEHOLDER" class="underline text-orange-400">https://discord.gg/PLACEHOLDER</a>
       </p>
     </div>
   </div>
